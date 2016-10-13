3f7c145de7c344866da083fa23b9b9d1135fe4d9
cheng10 <cheng10@ualberta.ca>
2016-10-13 02:18:34 -0600

    add models

--- web_api/rest_api/models.py
+++ web_api/rest_api/models.py
@@ -1,5 +1,5 @@
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -12,3 +12,62 @@ class Snippet(models.Model):
         ordering = ('created',)
 
 
+# class WarcUser(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class WarcFile(models.Model):
+    name = models.CharField(max_length=100, blank=True, default='')
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+
+
+class Document(models.Model):
+    DOC_TYPE = (
+        ('html', 'html file'),
+        ('pdf', 'pdf file'),
+        ('other', 'other file type'),
+    )
+    crawl_date = models.DateTimeField()
+    title = models.CharField(max_length=100, blank=True, default='')
+    type = models.CharField(max_length=50, choices=DOC_TYPE, default='other')
+    detail = models.TextField(blank=True, default='')  # detail description
+    link = models.TextField(blank=True, default='')  # way back server link
+    file = models.ForeignKey(WarcFile, on_delete=models.CASCADE, related_name='document')
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('crawl_date',)
+
+
+class Image(models.Model):
+    crawl_date = models.DateTimeField()
+    name = models.CharField(max_length=100, blank=True, default='')
+    detail = models.TextField(blank=True, default='')  # detail description, metadata?
+    link = models.TextField(blank=True, default='')  # way back server link
+    file = models.ForeignKey(WarcFile, on_delete=models.CASCADE, related_name='image')
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('crawl_date',)
+
+
+class Collection(models.Model):
+    warcuser = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, blank=True, default='')
+    detail = models.TextField(blank=True, default='')  # detail description
+    file = models.ManyToManyField(WarcFile)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)

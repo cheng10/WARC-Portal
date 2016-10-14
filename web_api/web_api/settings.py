1b039f96c80fff099349f9e39d5fd69c5446e739
cheng10 <cheng10@ualberta.ca>
2016-10-14 00:44:43 -0600

    using the MySQL DB on warc.tech instead of developmental sqlite3 DB

--- web_api/web_api/settings.py
+++ web_api/web_api/settings.py
@@ -80,10 +80,22 @@ WSGI_APPLICATION = 'web_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'warcdb',
+        'USER': 'warcuser',
+        'PASSWORD': 'mywarcpass',
+        'HOST': 'warc.tech',  # using our server's DB
+        'PORT': '3306',
     }
 }
 

f00be7c9ce18ac86df75ac62d352cf34bb7ee92a
Adriano Marini <marini@ualberta.ca>
2016-10-22 14:30:34 -0600

    Scala update #1 - Scala grammar

--- scripts.scala
+++ scripts.scala
@@ -4,16 +4,20 @@ import StringUtils._
 import java.sql.DriverManager
 import java.sql.Connection
 
-val r = RecordLoader.loadArchives("/directory/to/arc/file.arc.gz", sc)
-  .keepValidPages()
-  .map(r => r.getUrl)
-  .take(10)
+def main(args: Array[String]){
+  val r = RecordLoader.loadArchives("/directory/to/arc/file.arc.gz", sc)
+    .keepValidPages()
+    .map(r => r.getUrl)
+    .take(10)
+
+  println(r)
+}
 
 //http://alvinalexander.com/scala/scala-jdbc-connection-mysql-sql-select-example
 
 object ScalaJdbcConnectSelect {
 
-  def main(args: Array[String]) {
+  def database(args: Array[String]) {
     // connect to the database named "mysql" on the localhost
     val driver = "com.mysql.jdbc.Driver"
     val url = "jdbc:mysql://localhost/mysql"

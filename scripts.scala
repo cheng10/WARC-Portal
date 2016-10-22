196f02588d457630198e29fe1a8438bef76ad1d2
verunica <verunica@ualberta.ca>
2016-10-22 16:36:07 -0600

    Updated script -- Almost final version.

--- scripts.scala
+++ scripts.scala
@@ -1,44 +1,14 @@
+
 import org.warcbase.spark.matchbox._
 import org.warcbase.spark.rdd.RecordRDD._
 import StringUtils._
-import java.sql.DriverManager
-import java.sql.Connection
+import org.warcbase.spark.matchbox.TupleFormatter._
 
-def main(args: Array[String]){
-  val r = RecordLoader.loadArchives("/directory/to/arc/file.arc.gz", sc)
-    .keepValidPages()
-    .map(r => r.getUrl)
-    .take(10)
-
-  println(r)
-}
-
-//http://alvinalexander.com/scala/scala-jdbc-connection-mysql-sql-select-example
-
-object ScalaJdbcConnectSelect {
-
-  def database(args: Array[String]) {
-    // connect to the database named "mysql" on the localhost
-    val driver = "com.mysql.jdbc.Driver"
-    val url = "jdbc:mysql://localhost/mysql"
-    val username = "root"
-    val password = "root"
-
-    // there's probably a better way to do this
-    var connection:Connection = null
-
-    try {
-      // make the connection
-      Class.forName(driver)
-      connection = DriverManager.getConnection(url, username, password)
-
-      // create the statement, and run the select query
-      val statement = connection.createStatement()
-      for(a <- )
-      val resultSet = statement.executeQuery("INSERT INTO _ () VALUES ()")
-    } catch {
-      case e => e.printStackTrace
+object script {
+	def main(){
+		val r = RecordLoader.loadArchives("/home/ubuntu/warcbase/warcbase-core/src/test/resources/arc/example.arc.gz", sc)
+		.keepValidPages()
+		.map(r => (r.getCrawlDate, r.getUrl, RemoveHTML(r.getContentString), ExtractImageLinks(r.getUrl, r.getContentString)))
+		.saveAsTextFile("/home/ubuntu/me")
     }
-    connection.close()
-  }
 }

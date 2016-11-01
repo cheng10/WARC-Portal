eef002c71057b2b826cdf6096dcda0be86de10d4
cheng10 <cheng10@ualberta.ca>
2016-10-31 23:20:08 -0600

    try to record file names in the target dir using scala

--- scripts/job.scala
+++ scripts/job.scala
@@ -3,11 +3,14 @@ import org.warcbase.spark.rdd.RecordRDD._
 import StringUtils._
 import org.warcbase.spark.matchbox.TupleFormatter._
 import org.warcbase.spark.utils.JsonUtil
+import java.io._
 
 var warc="/mnt/md0/warc_tmp/"
-val r = RecordLoader.loadArchives(warc, sc)
-.keepValidPages()
-.map(r => (r.getCrawlDate, r.getDomain, r.getUrl, r.getContentString, ExtractImageLinks(r.getUrl, r.getContentString)))
-.map(r => JsonUtil.toJson(r))
-.saveAsTextFile("/mnt/md0/spark_out")
-exit()
+val array = new File(warc).list
+println(array.deep.mkString("\n"))
+//val r = RecordLoader.loadArchives(warc, sc)
+//.keepValidPages()
+//.map(r => (r.getCrawlDate, r.getDomain, r.getUrl, r.getContentString, ExtractImageLinks(r.getUrl, r.getContentString)))
+//.map(r => JsonUtil.toJson(r))
+//.saveAsTextFile("/mnt/md0/spark_out")
+//exit()

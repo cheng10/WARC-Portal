6543c917e56c986e4de2ef68a069b0a9825dbcf1
cheng10 <cheng10@ualberta.ca>
2016-11-02 04:17:42 -0600

    store image

--- web_api/rest_api/management/commands/parsedocs.py
+++ web_api/rest_api/management/commands/parsedocs.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from pprint import pprint
 from datetime import datetime
 from django.core.management.base import BaseCommand, CommandError
-from ...models import Document, WarcFile
+from ...models import Document, WarcFile, Image
 
 DIR = "/mnt/md0/spark_out/"
 
@@ -15,6 +15,7 @@ class Command(BaseCommand):
                 if outfile.startswith('part'):
                     print "Parsing..." + warc_file_name
                     f = open(DIR+warc_file_name+'/'+outfile)
+                    # record warc file name
                     warc = WarcFile.objects.create(name=warc_file_name)
                     for line in f:
                         try:
@@ -25,69 +26,81 @@ class Command(BaseCommand):
                         except:
                             print "Error parsing JSON"
 
-                        # only when the content is valid, push them to DB
-                        # if data[2].startswith('HTTP/1.1 200') or data[2].startswith('HTTP/1.0 200'):
-                        # if data[2].split()[1] == '200':
-                        if 1:
-                            # fetch webpage title
-                            domain = data[1]
-                            soup = BeautifulSoup(data[3], "html.parser")
+                        # parse crawl date
+                        crawl_date = datetime.strptime(data[0], '%Y%m%d').strftime("%Y-%m-%d"),
+
+                        # fetch domain
+                        domain = data[1]
+
+                        # fetch web page title and content
+                        soup = BeautifulSoup(data[3], "html.parser")
+                        try:
+                            title = soup.title.string
+                            title = title.replace("<title>", "")
+                        except AttributeError:
+                            title = ''
+                        title = title[:254]
+                        text = soup.get_text()
+                        text = re.sub(' +', ' ', text)  # remove spaces
+                        text = text.replace('\n', ' ').replace('\r', '')
+                        if title == '':
+                            title = domain
+                        if title == '':
+                            title = 'none'
+
+                        # fetch publication date using alchemy api
+                        url = data[2]
+                        payload = {
+                            'url': url,
+                            'apikey': '7f9faed9fb0243ad50831864294e108fe5d49529',
+                            'outputMode': 'json'
+                        }
+                        api_url = "http://gateway-a.watsonplatform.net/calls/url/URLGetPubDate"
+                        r = requests.get(api_url, params=payload)
+                        # print r.url
+                        # pprint(r.json())
+                        if r.json()['status'] == 'OK':
                             try:
-                                title = soup.title.string
-                                title = title.replace("<title>", "")
-                            except AttributeError:
-                                title = ''
-                            title = title[:254]
-                            text = soup.get_text()
-                            text = re.sub(' +', ' ', text)  # remove spaces
-                            text = text.replace('\n', ' ').replace('\r', '')
-                            # print title
-                            if title == '':
-                                title = domain
-                            if title == '':
-                                title = 'none'
-
-                            # fetch publication date using alchemyapi
-                            url = data[2]
-                            payload = {
-                                'url': url,
-                                'apikey': '7f9faed9fb0243ad50831864294e108fe5d49529',
-                                'outputMode': 'json'
-                            }
-                            api_url = "http://gateway-a.watsonplatform.net/calls/url/URLGetPubDate"
-                            r = requests.get(api_url, params=payload)
-
-                            # print r.url
-                            # pprint(r.json())
-                            if r.json()['status'] == 'OK':
-                                try:
-                                    date = r.json()['publicationDate']['date'].replace('T', '')
-                                except KeyError:
-                                    date = '19700101000000'
-                                try:
-                                    conf_str = r.json()['publicationDate']['confident']
-                                except KeyError:
-                                    conf_str = 'no'
-                                if conf_str == 'no':
-                                    confident = False
-                                else:
-                                    confident = True
-                                if date == '':
-                                    date = '19700101000000'
-                            # populate the field with unix zero time when pub_date unavailable
-                            # this may cause by exceeding daily alchemyapi query limit
-                            else:
+                                date = r.json()['publicationDate']['date'].replace('T', '')
+                            except KeyError:
                                 date = '19700101000000'
+                            try:
+                                conf_str = r.json()['publicationDate']['confident']
+                            except KeyError:
+                                conf_str = 'no'
+                            if conf_str == 'no':
                                 confident = False
+                            else:
+                                confident = True
+                            if date == '':
+                                date = '19700101000000'
+                        # populate the field with unix zero time when pub_date unavailable
+                        # this may cause by exceeding daily alchemy api query limit
+                        else:
+                            date = '19700101000000'
+                            confident = False
 
-                            Document.objects.create(
-                                title=title,
-                                domain=domain,
-                                file=warc,
-                                pub_date=datetime.strptime(date, '%Y%m%d%H%M%S'),
-                                pub_date_confident=confident,
-                                crawl_date=datetime.strptime(data[0], '%Y%m%d').strftime("%Y-%m-%d"),
-                                link=data[2],
-                                content=text.encode('unicode_escape'),
-                                type='html',  # document type, to do
-                            )
+                        # parse and store images
+                        if data[4]:
+                            for link in data[4]:
+                                name = link.split('/')[-1]
+                                Image.objects.create(
+                                    crawl_date=crawl_date,
+                                    name=name,
+                                    detail='',
+                                    link=link,
+                                    file=warc,
+                                )
+
+                        # store documents
+                        Document.objects.create(
+                            title=title,
+                            domain=domain,
+                            file=warc,
+                            pub_date=datetime.strptime(date, '%Y%m%d%H%M%S'),
+                            pub_date_confident=confident,
+                            crawl_date=crawl_date,
+                            link=data[2],
+                            content=text.encode('unicode_escape'),
+                            type='html',  # document type, to do
+                        )

c3b1679f415af5080c2831f207d22178adc5e0c5
cheng10 <cheng10@ualberta.ca>
2016-11-04 01:59:23 -0600

    fix site link scala script

--- scripts/site_link.scala
+++ scripts/site_link.scala
@@ -1,13 +1,22 @@
 import org.warcbase.spark.matchbox._
 import org.warcbase.spark.rdd.RecordRDD._
 import StringUtils._
+import org.warcbase.spark.matchbox.TupleFormatter._
+import org.warcbase.spark.utils.JsonUtil
+import java.io._
 
-val links = RecordLoader.loadArchives("/collections/webarchives/geocities/warcs/", sc)
-  .keepValidPages()
-  .flatMap(r => ExtractLinks(r.getUrl, r.getContentString))
-  .map(r => (ExtractDomain(r._1).removePrefixWWW(), ExtractDomain(r._2).removePrefixWWW()))
-  .filter(r => r._1 != "" && r._2 != "")
-  .countItems()
-  .filter(r => r._2 > 5)
+var warc="/mnt/md0/warc_tmp/"
+val fileList = new File(warc).list
 
-links.saveAsTextFile("geocities-links-all/")
+for (file <- fileList) {
+    val r = RecordLoader.loadArchives(warc+file, sc)
+    .keepValidPages()
+    .flatMap(r => ExtractLinks(r.getUrl, r.getContentString))
+    .map(r => (ExtractDomain(r._1).removePrefixWWW(), ExtractDomain(r._2).removePrefixWWW()))
+    .filter(r => r._1 != "" && r._2 != "")
+    .countItems()
+    .filter(r => r._2 > 5)
+    .saveAsTextFile("/mnt/md0/spark_sitelink/"+file)
+    }
+
+exit()
